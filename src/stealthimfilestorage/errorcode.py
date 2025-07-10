d25875bf3b8f09aefbb6c55869fc013adcd17af7
cxykevin <cxykevin@yeah.net>
2025-07-10 18:16:17 +0800

    更新版本号

---
+++ src/stealthimfilestorage/errorcode.py
@@ -0,0 +1,13 @@
+# 8xx 成功
+SUCCESS = 800  # 成功处理请求
+
+# 9xx 公共失败
+SERVER_FAILED = 900  # 服务器未捕获异常
+SERVER_REFUSED = 901  # 服务器未能成功处理请求
+SERVER_INTERNAL_COMPONENT_ERROR = 902  # 服务器运行时错误
+SERVER_INTERNAL_NETWORK_ERROR = 903  # 服务器组件通信错误
+SERVER_OVERLOAD = 904  # 服务器过载
+SERVER_LIMITED = 905  # 请求过大
+SERVER_ERROR = 906  # 其它问题
+
+# 21xx FileStorage 失败
--- src/stealthimfilestorage/server.py
+++ src/stealthimfilestorage/server.py
@@ -2,6 +2,7 @@ from . import bind_server as serv
 from .proto import filestorage_pb2
 from . import config
 from . import storage
+from . import errorcode
 from typing import Any
 import asyncio
 
@@ -19,32 +20,32 @@ async def Ping(_: Any) -> filestorage_pb2.Pong:
 async def GetFile(data: filestorage_pb2.GetFileRequest) -> filestorage_pb2.GetFileResponse:
     try:
         dt = await storage.get(data.hash, data.block)
-        return filestorage_pb2.GetFileResponse(result=filestorage_pb2.Result(code=0, msg=""), block_data=dt)
+        return filestorage_pb2.GetFileResponse(result=filestorage_pb2.Result(code=errorcode.SUCCESS, msg=""), block_data=dt)
     except Exception as e:
-        return filestorage_pb2.GetFileResponse(result=filestorage_pb2.Result(code=1, msg=str(e)), block_data=b"")
+        return filestorage_pb2.GetFileResponse(result=filestorage_pb2.Result(code=errorcode.SERVER_INTERNAL_COMPONENT_ERROR, msg=str(e)), block_data=b"")
 
 
 @serv.serve
 async def SaveFile(data: filestorage_pb2.SaveFileRequest) -> filestorage_pb2.SaveFileResponse:
     try:
         await storage.save(data.block_data, data.hash, data.block)
-        return filestorage_pb2.SaveFileResponse(result=filestorage_pb2.Result(code=0, msg=""))
+        return filestorage_pb2.SaveFileResponse(result=filestorage_pb2.Result(code=errorcode.SUCCESS, msg=""))
     except Exception as e:
-        return filestorage_pb2.SaveFileResponse(result=filestorage_pb2.Result(code=1, msg=str(e)))
+        return filestorage_pb2.SaveFileResponse(result=filestorage_pb2.Result(code=errorcode.SERVER_INTERNAL_COMPONENT_ERROR, msg=str(e)))
 
 
 @serv.serve
 async def RemoveBlock(data: filestorage_pb2.RemoveBlockRequest) -> filestorage_pb2.RemoveBlockResponse:
     asyncio.create_task(storage.delete(data.block_hash))
-    return filestorage_pb2.RemoveBlockResponse(result=filestorage_pb2.Result(code=0, msg=""))
+    return filestorage_pb2.RemoveBlockResponse(result=filestorage_pb2.Result(code=errorcode.SUCCESS, msg=""))
 
 
 @serv.serve
 async def GetUsage(data: filestorage_pb2.GetUsageRequest) -> filestorage_pb2.GetUsageResponse:
-    return filestorage_pb2.GetUsageResponse(result=filestorage_pb2.Result(code=0, msg=""), usage=storage.usage, total=config.Max_Block)
+    return filestorage_pb2.GetUsageResponse(result=filestorage_pb2.Result(code=errorcode.SUCCESS, msg=""), usage=storage.usage, total=config.Max_Block)
 
 
 @serv.serve
 async def Reload(data: filestorage_pb2.ReloadRequest) -> filestorage_pb2.ReloadResponse:
     asyncio.create_task(config.reload_cfg())
-    return filestorage_pb2.ReloadResponse(result=filestorage_pb2.Result(code=0, msg=""))
+    return filestorage_pb2.ReloadResponse(result=filestorage_pb2.Result(code=errorcode.SUCCESS, msg=""))
